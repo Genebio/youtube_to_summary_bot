37e71fa7e5856a6f67ea73952f5ac35f497fad3a
Evgeniy Tikhonov <evgeniy.tikhonov@bostongene.com>
2024-10-02 15:06:12 +0400

    add user model

--- models/user_model.py
+++ models/user_model.py
@@ -1,16 +1,16 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from utils.datetime_utils import get_formatted_time
-from .base import Base
+from models.base import Base
 
 class User(Base):
     __tablename__ = 'users'
     
     user_id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(100), unique=True, nullable=False)
+    username = Column(String(50), unique=True, nullable=True)
     first_name = Column(String(100))
     last_name = Column(String(100))
-    language_code = Column(String(10))
+    language_code = Column(String(10), default='en')
     current_subscription_status = Column(Boolean, default=False)
     created_at = Column(DateTime, default=get_formatted_time())
     
