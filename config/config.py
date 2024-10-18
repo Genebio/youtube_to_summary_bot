b15f81a8bc4929cfa8da68e64b8f6a65e09b66b5
Evgeniy Tikhonov <evgeniy.tikhonov@bostongene.com>
2024-10-18 18:20:47 +0400

    update DB connection deatils for DigitalOcean DB

--- config/config.py
+++ config/config.py
@@ -18,22 +18,22 @@ OPENAI_API_KEY = get_secret("OPENAI_API_KEY", "1")
 if not OPENAI_API_KEY:
     raise ValueError("No OPENAI_API_KEY found in environment variables.")
 
-DB_USER = get_secret("DB_USER", "1")
+DB_USER = get_secret("DB_USER", "2")
 logger.info(f"DB_USER: {DB_USER}")
 if not DB_USER:
     raise ValueError("No DB_USER found in environment variables.")
 
-DB_PASS = urllib.parse.quote(get_secret("DB_PASS", "1"))
+DB_PASS = urllib.parse.quote(get_secret("DB_PASS", "2"))
 logger.info(f"DB_PASS: {DB_PASS}")
 if not DB_PASS:
     raise ValueError("No DB_PASS found in environment variables.")
 
-DB_NAME = get_secret("DB_NAME", "1")
+DB_NAME = get_secret("DB_NAME", "2")
 logger.info(f"DB_NAME: {DB_NAME}")
 if not DB_NAME:
     raise ValueError("No DB_NAME found in environment variables.")
 
-DB_HOST = get_secret("DB_HOST", "2")
+DB_HOST = get_secret("DB_HOST", "4")
 logger.info(f"DB_HOST: {DB_HOST}")
 if not DB_HOST:
     raise ValueError("No DB_HOST found in environment variables.")
